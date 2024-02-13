b7ed2d8d49ee1ae9f99ff57ade00997c2de3d8da
s10502 <jason.bowerman@utsouthwestern.edu>
2024-02-13 13:59:29 -0600

    padding is set to same so dimensions stay the same

--- bin/basic_MGMT_01_pytorch/mgmt_class.py
+++ bin/basic_MGMT_01_pytorch/mgmt_class.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch import flatten
 import numpy as np
 import pandas as pd
 
@@ -12,21 +13,21 @@ class MGMTModel(nn.Module):
     """
     def __init__(self, input_size, output_size):
         super(MGMTModel, self).__init__()
-        # 240x240 conv = (240-25)/1 + 1 = 216x216
-        self.conv1 = nn.Conv2d(1, 8, 25)
-        # 216x216 max pool = 108x108
+        # 240x240 image
+        self.conv1 = nn.Conv2d(1, 8, 5, padding='same')
+        # 240x240 mp = 120x120
         self.pool = nn.MaxPool2d(2, 2)
-        # 108x108 conv = (108-25)/1 + 1 = 84x84
-        self.conv2 = nn.Conv2d(8, 16, 25) 
-        # 84x84 use 16 featues * 84x84 image
-        self.fc1 = nn.Linear(16 * 84 * 84, 128)
+        # 120x120 image
+        self.conv2 = nn.Conv2d(8, 16, 5, padding='same') 
+        # 120X120 use 16 featues * 120x120 image
+        self.fc1 = nn.Linear(16 * 120 * 120, 128)
         self.fc2 = nn.Linear(128, 32)
         self.fc3 = nn.Linear(32, 10) # output layer
 
     def forward(self, x):
         x = self.pool(F.softmax(self.conv1(x)))
         x = self.pool(F.softmax(self.conv2(x)))
-        # x = x.view(-1, 16 * 84 * 84)
+        x = flatten(x, 1) # flatten all dimensions except batch
         x = F.softmax(self.fc1(x))
         x = F.softmax(self.fc2(x))
         x = self.fc3(x)
