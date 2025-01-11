a685a2098355bb9599a7c98db213c5c32a8cfda9
Jason Bowerman <qwaddles@qwaddles-lilBlackLap.qnet>
2025-01-11 16:43:40 -0600

    it works

--- ConvNets/UNet/unet_segmentation.py
+++ ConvNets/UNet/unet_segmentation.py
@@ -9,18 +9,19 @@ import torch.nn as nn
 from torchvision import models, datasets
 from torchvision.transforms import ToTensor
 from torch.nn.functional import relu
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, random_split, Dataset
 from torchvision.io import read_image
 
 class UNet(nn.Module):
     def __init__(self, n_classes):
         super().__init__()
-        self.channels = [64, 128, 256, 512, 1024]
+        self.channels = [64, 128, 256, 512]
         self.encoder = Encoder(3, self.channels[:-1])
         self.conv1 = nn.Conv2d(self.channels[-1-1], self.channels[-1], kernel_size=3, padding=1) 
         self.conv2 = nn.Conv2d(self.channels[-1], self.channels[-1], kernel_size=3, padding=1) 
-        self.decoder = Decoder(self.channels[:-1][::-1])
-        self.conv_out = nn.Conv2d(56, n_classes, kernel_size=1)
+        self.decoder = Decoder(self.channels[::-1])
+        self.conv_out = nn.Conv2d(self.channels[0], n_classes, kernel_size=1)
 
     def forward(self, x):
         v, skips = self.encoder(x)
@@ -61,7 +62,7 @@ class Decoder(nn.Module):
             self.up_convs.append(UpConvBlock(self.channels[i-1], self.channels[i]))
 
     def forward(self, x, skips):
-        for i in len(self.channels):
+        for i in range(len(self.channels)-1):
             x = self.up_convs[i](x, skips[i])
         return x
 
@@ -76,7 +77,7 @@ class ConvBlock(nn.Module):
         out = relu(self.conv1(x))
         skip = relu(self.conv2(out))
         val = self.pool(skip)
-        print(val.shape)
+        # print(val.shape)
         return val, skip 
     
 class UpConvBlock(nn.Module):
@@ -88,8 +89,19 @@ class UpConvBlock(nn.Module):
     
     def forward(self, x, skip):
         upconv = self.conv_t(x)
-        print(f"output: {upconv.shape}")
-        print(f"skip: {skip.shape}")
+        # print(f"output: {upconv.shape}")
+        # print(f"skip: {skip.shape}")
+
+        if upconv.shape[2:] != skip.shape[2:]:
+            # print("resizing")
+            upconv = F.interpolate(upconv, size=skip.shape[2:], mode="bilinear", align_corners=False)
+        # if upconv.shape[2] != skip.shape[2] or upconv.shape[3] != skip.shape[3]:
+        #     #center crop if the skip and output don't match before concatinating
+        #     print("cropping...")
+        #     _, _, h, w = skip.shape
+        #     delta_h = (h - upconv.shape[2]) // 2
+        #     delta_w = (w - upconv.shape[3]) // 2
+        #     skip = skip[:, :, delta_h:delta_h + upconv.shape[2], delta_w:delta_w + upconv.shape[3]]
         val = torch.cat([upconv, skip], dim=1)
         val = relu(self.conv1(val))
         val = relu(self.conv2(val))
@@ -112,11 +124,13 @@ class CatDataset(Dataset):
         mask_path = os.path.join(self.mask_dir, self.fnames[idx])
         mask = read_image(mask_path)
         mask[mask > 0] = 1
+        # print(f"image shape: {image.shape}")
+        # print(f"mask shape: {mask.shape}")
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, mask
+        return image.float(), mask.float()
 
 def get_data():
     with open("info.yaml", "r") as file:
@@ -128,13 +142,15 @@ def get_data():
 
 if __name__ == "__main__":
     data = get_data()
+    img, mask = data[0]
+    
     train_count = int(0.8 * len(data))
     valid_count = len(data) - train_count
     # Randomly split the dataset
     train_dataset, val_dataset = random_split(data, [train_count, valid_count])
 
-    print(len(train_dataset))
-    print(len(val_dataset))
+    # print(len(train_dataset))
+    # print(len(val_dataset))
     # (Optional) Create DataLoaders for batching
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=1)
@@ -151,19 +167,22 @@ if __name__ == "__main__":
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters())
     num_epochs = 10
+    model_path = "my_model.chk"
 
     for epoch in range(num_epochs):
         model.train()
         for batch_idx, (data,target) in enumerate(train_loader):
-            print(data.shape)
+            # print(data.shape)
             optimizer.zero_grad()
             output = model(data)
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
+            print(f"{(batch_idx * len(data)):5}/{len(train_dataset):5}: {(((batch_idx*len(data))/len(train_dataset))*100):4}", end="\r")
             if batch_idx % 100 == 0:
-                print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_data.dataset)}]\tLoss: {loss.item():.6f}')
+                print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_dataset)}]\tLoss: {loss.item():.6f}')
 
+        torch.save(model.state_dict(), model_path)
         # Validation phase
         model.eval()
         val_loss = 0
